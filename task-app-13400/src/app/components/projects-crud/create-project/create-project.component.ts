0dbce53660099b97b7b632d4cf645f36a8e31559
wiut00013400 <wiut13400@gmail.com>
2024-03-20 10:44:15 +0500

    fix design, colors, texts

--- task-app-13400/src/app/components/projects-crud/create-project/create-project.component.ts
+++ task-app-13400/src/app/components/projects-crud/create-project/create-project.component.ts
@@ -43,4 +43,7 @@ export class CreateProjectComponent {
     }
   });
   };
+  cancel(){
+    this.router.navigateByUrl("home");
+  }
 }
--- task-app-13400/src/app/components/tasks-crud/create/create.component.ts
+++ task-app-13400/src/app/components/tasks-crud/create/create.component.ts
@@ -58,4 +58,8 @@ export class CreateTaskComponent {
       }
     });
   };
+
+  cancel() {
+    this.router.navigateByUrl("home")
+  }
 }
